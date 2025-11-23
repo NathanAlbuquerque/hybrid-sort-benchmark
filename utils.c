37b018c8cb40ccbc157c7edb1af5e51d59cd410f
Nathan Albuquerque <nathan.nra@outlook.com>
2025-11-23 16:29:14 -0300

    feat: create h files and main function

---
+++ utils.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "utils.h"
+
+void gera_vetor(int *arr, int n, int tipo) {
+    // Implementar aqui...
+}
+
+void copia_vetor(int *dest, int *src, int n) {
+    // Implementar aqui...
+}
+
+void imprime_vetor(int *arr, int n) {
+    // Implementar aqui...
+}
